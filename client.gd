84802b5171026077812544583eec7b3975c5aa40
Shuflduf <shuflduf@gmail.com>
2025-05-11 17:43:53 -0600

    part 1 idk

--- client.gd
+++ client.gd
@@ -4,6 +4,6 @@ const ADDRESS = "127.0.0.1"
 const PORT = 7890
 
 func _ready() -> void:
-	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(ADDRESS, PORT)
-	multiplayer.multiplayer_peer = peer
+    var peer = ENetMultiplayerPeer.new()
+    peer.create_client(ADDRESS, PORT)
+    multiplayer.multiplayer_peer = peer
