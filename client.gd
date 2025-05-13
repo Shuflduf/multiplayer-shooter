849bea36d45aa8dc8c52ad165d7311f14a70417c
Shuflduf <shuflduf@gmail.com>
2025-05-12 19:37:49 -0600

    added chat part 2 idk

--- client.gd
+++ client.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+@onready var offset = Time.get_ticks_msec()
+
 const ADDRESS = "127.0.0.1"
 const PORT = 7890
 
@@ -7,3 +9,17 @@ func _ready() -> void:
     var peer = ENetMultiplayerPeer.new()
     peer.create_client(ADDRESS, PORT)
     multiplayer.multiplayer_peer = peer
+
+func _on_text_box_text_submitted(new_text: String) -> void:
+    %TextBox.text = ""
+    send_message.rpc(new_text)
+    #send_message.rpc_id(1, new_text)
+
+@rpc("any_peer", "call_local")
+func send_message(message: String) -> void:
+    var sender_id = multiplayer.get_remote_sender_id()
+    var new_message = Label.new()
+    var format_string = "[ %d ] %s - %s"
+    var actual_string = format_string % [ Time.get_ticks_msec() - offset, sender_id, message ]
+    new_message.text = actual_string
+    %Messages.add_child(new_message)
--- server.gd
+++ server.gd
@@ -1,6 +1,7 @@
 extends Control
 
 const PORT = 7890
+@onready var offset = Time.get_ticks_msec()
 
 func _ready() -> void:
     log_event("Server Started")
@@ -10,12 +11,10 @@ func _ready() -> void:
     multiplayer.peer_connected.connect(_on_client_connected)
 
 func log_event(event: String):
-    var new_log = RichTextLabel.new()
-    new_log.fit_content = true
-    new_log.bbcode_enabled = true
+    var new_log = Label.new()
 
-    var format_string = "[b][ %d ][/b] - %s"
-    var actual_string = format_string % [ Time.get_ticks_msec(), event ]
+    var format_string = "[ %d ] - %s"
+    var actual_string = format_string % [ Time.get_ticks_msec() - offset, event ]
 
     new_log.text = actual_string
     %Logs.add_child(new_log)
@@ -23,7 +22,10 @@ func log_event(event: String):
 func _on_client_connected(id):
     log_event("Client %d connected" % id)
 
+@rpc("any_peer", "call_local")
+func send_message(message: String) -> void:
+    log_event("%s - %s" % [multiplayer.get_remote_sender_id(), message])
 
-func _on_command_input_text_submitted(new_text: String) -> void:
-    log_event(new_text)
-    %CommandInput.text = ""
+#func _on_command_input_text_submitted(new_text: String) -> void:
+    #log_event(new_text)
+    #%CommandInput.text = ""
